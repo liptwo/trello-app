8f4b3a96d343821483b293467c19d4df86082379
liptwo <liptwo0@gmail.com>
2025-05-14 18:21:40 +0700

    refactor_project

---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box'
+import Theme from '../../../theme'
+
+const BoardContent = () => {
+  return (
+    <Box sx={{
+      backgroundColor:'primary.main',
+      width: '100%',
+      height: (theme) => `calc(100vh - ${theme.trello.headHeight + theme.trello.boardBarHeight}px)`,
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      Main
+    </Box>
+  )
+}
+
+export default BoardContent
